65825314930b584b5bb9f7370fadef80c2f06ca1
Thiago <mctks2@hotmail.com>
2024-05-21 16:01:27 -0300

    final project

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
+const elementos = {
+    mensagemErro: document.getElementById('erro'),
+    cidade: document.getElementById('cidade'),
+    logradouro: document.getElementById('endereco'),
+    estado: document.getElementById('estado'),
+    campoCep: document.getElementById('cep'),
+    bairro: document.getElementById('bairro'),
+    complemento: document.getElementById('complemento')
+};
+
 async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
-    mensagemErro.innerHTML = "";
+    elementos.mensagemErro.innerHTML = "";
+
     try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        if (consultaCEPConvertida.erro) {
-            throw Error('CEP não existente!');
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const dadosEndereco = await resposta.json();
+
+        if (dadosEndereco.erro) {
+            throw new Error('CEP não encontrado. Por favor, verifique o CEP digitado.');
         }
-        var cidade = document.getElementById('cidade');
-        var logradouro = document.getElementById('endereco');
-        var estado = document.getElementById('estado');
 
-        cidade.value = consultaCEPConvertida.localidade;
-        logradouro.value = consultaCEPConvertida.logradouro;
-        estado.value = consultaCEPConvertida.uf;
-
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
+        exibirEndereco(dadosEndereco);
+        return dadosEndereco;
     } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
-        console.log(erro);
+        elementos.mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.error(erro);
     }
 }
 
-var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+function exibirEndereco(endereco) {
+    elementos.cidade.value = endereco.localidade;
+    elementos.logradouro.value = endereco.logradouro;
+    elementos.estado.value = endereco.uf;
+    elementos.bairro.value = endereco.bairro;
+    elementos.complemento.value = endereco.complemento;
+
+    console.log(endereco);
+}
+
+
+elementos.campoCep.addEventListener("focusout", () => buscaEndereco(elementos.campoCep.value));
